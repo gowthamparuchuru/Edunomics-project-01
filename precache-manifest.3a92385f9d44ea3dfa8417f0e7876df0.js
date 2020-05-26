ece27ddee5c5ef65300417a49a57b35bf0721fbf
pvsgowtham <pvsg737@gmail.com>
2020-05-26 12:11:32 +0530

    Updates

---
+++ precache-manifest.3a92385f9d44ea3dfa8417f0e7876df0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a63770b0816019d45babaf1624db64",
+    "url": "/edunomics-project-01/index.html"
+  },
+  {
+    "revision": "71be87d0eeea060fe42e",
+    "url": "/edunomics-project-01/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "a4f4659ac20b59489175",
+    "url": "/edunomics-project-01/static/css/main.3d0f3621.chunk.css"
+  },
+  {
+    "revision": "71be87d0eeea060fe42e",
+    "url": "/edunomics-project-01/static/js/2.7ea4a784.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/edunomics-project-01/static/js/2.7ea4a784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f4659ac20b59489175",
+    "url": "/edunomics-project-01/static/js/main.972c2cae.chunk.js"
+  },
+  {
+    "revision": "1cb2a5509c2353229a12",
+    "url": "/edunomics-project-01/static/js/runtime-main.0a1bed06.js"
+  }
+]);
